43f111f2134e75cd0bc85c7b9a5e72394eed8f95
Jacob Stanton <jacob@webfitters.com>
2024-03-04 11:21:08 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("world");
+    format!("Hello {:?}", &name)
+}
+
+async fn health_check(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
 }
