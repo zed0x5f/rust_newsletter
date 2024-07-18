65c3fab88a504e1ec8bdfe97ed428987c8f86086
Jacob Stanton <jacob@webfitters.com>
2024-07-18 19:00:15 -0400

    wip

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -1,4 +1,4 @@
-use validator::validate_email;
+use validator::ValidateEmail;
 
 #[derive(Debug)]
 pub struct SubscriberEmail(String);
@@ -24,13 +24,14 @@ mod tests {
     use claims::assert_err;
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
+    use quickcheck::Gen;
 
     #[derive(Debug, Clone)]
     struct ValidEmailFixture(String);
 
     impl quickcheck::Arbitrary for ValidEmailFixture {
-        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
-            let email = SafeEmail().fake_with_rng(g);
+        fn arbitrary(g: &mut Gen) -> Self {
+            let email = SafeEmail().fake_with_rng();
             Self(email)
         }
     }
